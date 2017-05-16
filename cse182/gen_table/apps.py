8a7194fbad921ad80ac2db820fa6602faf622edf
kkhaichau <kkhaichau@gmail.com>
2017-05-16 16:37:51 -0700

    started models

---
+++ cse182/gen_table/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GenTableConfig(AppConfig):
+    name = 'gen_table'
---
+++ cse182/gen_table/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Annotation(models.Model):
+    """
+        Model for a genomic annotation
+    """
