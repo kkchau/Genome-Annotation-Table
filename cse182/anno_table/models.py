8201d51d4c6f8cf6d00313610177c20b5e34eb50
kkhaichau <kkhaichau@gmail.com>
2017-05-17 21:06:14 -0700

    general comments in views and models

--- cse182/anno_table/models.py
+++ cse182/anno_table/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Annotation(models.Model):
+    """
+        Annotation model
+        Field corresponds to possible annotation sources, including comments
+    """
     name = models.CharField(max_length=200, verbose_name='sequence_identifer')
     blast = models.CharField(max_length=200, verbose_name='blast_hit')
     pfam = models.CharField(max_length=200, verbose_name='pfam_hit')
--- cse182/anno_table/views.py
+++ cse182/anno_table/views.py
@@ -5,5 +5,10 @@ from .models import Annotation
 # Create your views here.
 
 def base(request):
+    """
+        Base view
+        Simply gathers annotations and collects that set and the template into
+            the context
+    """
     annotations = Annotation.objects.all()
     return render(request, 'anno_table/base.html', {'annotations': annotations)
