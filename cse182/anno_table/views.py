25f87dd4592101a78898b199a634c75d6dfdafcd
kkhaichau <kkhaichau@gmail.com>
2017-05-17 20:59:50 -0700

    table done

--- cse182/anno_table/views.py
+++ cse182/anno_table/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Annotation
 
 # Create your views here.
 
 def base(request):
-    return render(request, 'anno_table/base.html')
+    annotations = Annotation.objects.all()
+    return render(request, 'anno_table/base.html', {'annotations': annotations)
