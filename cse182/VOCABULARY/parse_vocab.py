f974248f2127740dd1fec948f3e9807160f4f74e
kkhaichau <kkhaichau@gmail.com>
2017-06-04 16:03:54 -0700

    vocab work

--- cse182/VOCABULARY/parse_vocab.py
+++ cse182/VOCABULARY/parse_vocab.py
@@ -5,7 +5,53 @@
 
 
 import pandas as pd
-from goparse import goParse
+from collections import defaultdict
+
+
+def processGoTerm(goTerm):
+    """
+        Process passed goTerm defaultdict object into manageable dictionary
+        with collapsed single-item lists
+    """
+    proc = dict(goTerm)
+    for key, value in proc.items():
+        if len(value) == 0:
+            proc[key] = value[0]        # collapse single-item lists
+    return proc
+
+
+def goParse(filename):
+    """
+        Parse the given GO file for term objects
+        Generates Gene Onotology terms
+    """
+    with open(filename, 'r') as infile:
+        currentGoTerm = None
+        for line in infile:
+
+            line = line.strip()
+            if not line: continue       # skip empty lines
+
+            # goTerm defaultdict
+            if line == '[Term]':
+                if currentGoTerm: 
+                    yield processGoTerm(currentGoTerm)
+                currentGoTerm = defaultdict(list)
+
+            elif line == '[Typedef]':
+                currentGoTerm = None
+
+            # try to parse goTerm entry
+            else:
+                if currentGoTerm is None: 
+                    continue
+
+                # add information
+                key, sep, value = line.partition(':')
+                currentGoTerm[key].append(value.strip())
+
+        if currentGoTerm is not None:
+            yield processGoTerm(currentGoTerm)
 
 
 def syn_gen(filename):
@@ -49,7 +95,8 @@ def main():
                     go_list.append(synonyms[word])
         record['Gene Ontology'] = list(set(go_list))
     
-    source_data.to_csv('aaa.csv', index=False)
+    # print for now, debugging
+    print(source_data)
 
 
 if __name__ == '__main__':
